33b93c6916d197dc96304d317f7dfa87b48bb8d0
Adarsh Raj <adarshrj62@gmail.com>
2023-01-17 18:01:40 +0530

    Pill Improved

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -81,9 +81,9 @@ class Header extends Component{
                    
                         <Nav className="ml-auto" navbar>
                             <NavItem>
-                                <Badge  className="login-btn " pill onClick={this.toggleModal}>
+                                <Button  className="login-btn " pill onClick={this.toggleModal}>
                                     <span className='mt-2 mb-2 ml-4 mr-4 fa fa-sign-in fa-lg'>Login</span>
-                                </Badge>
+                                </Button>
                             </NavItem>
                         </Nav>
                     </Collapse>
